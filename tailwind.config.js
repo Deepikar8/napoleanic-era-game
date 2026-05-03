679091a70df962c7d854f3df5dac3d08bdd6d556
Deepika Rudramurthy <avalanche05@gmail.com>
2026-05-03 14:48:26 +0800

    chore: bootstrap Vite + React + TS + Tailwind + Vitest

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        parchment: '#f5f0e6',
+        parchmentDark: '#e8dfc3',
+        ink: '#2a2018',
+        french: '#2c5aa0',
+        austrian: '#ece4d0',
+        russian: '#4a7a4a',
+        gilt: '#d4a017',
+      },
+      fontFamily: {
+        serif: ['"Cormorant Garamond"', 'Georgia', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
